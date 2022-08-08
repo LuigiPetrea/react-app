c96ff3d05f83793571b935ff0ca9f0f03f26f118
LuigiPetrea <levipetrea08@gmail.com>
2022-08-08 14:53:11 +0300

    modified resolution logo

--- src/page/about.js
+++ src/page/about.js
@@ -6,7 +6,7 @@ function About() {
         <div className="content">
              <h1 className="title">About the React Library</h1>
              <article className="box-about">
-                <img className={"img-float"} src={imgLogoReact} alt="logoReact" width={"25%"}/>
+                <img className={"img-float"} src={imgLogoReact} alt="logoReact" width={"20%"}/>
                 <aside className="content-txt-float">
                 <p>
                     React (also known as <strong>React.js</strong> or <strong>ReactJS</strong>) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page, mobile, or server-rendered applications with frameworks like Next.js. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality.
